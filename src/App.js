ca0bd8f3942115163336f19f8d9563f44b5012fc
Gergo Kondor <kondor.gerg@gmail.com>
2021-09-03 14:46:32 +0100

    Navbar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <h2>Portfolio Project...</h2>
+    <Navbar />
   );
 }
 
