2e31479147273fb4655c0457d6ebc9c71da42077
Gergo Kondor <kondor.gerg@gmail.com>
2021-09-11 17:27:54 +0100

    Services completed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Particles from 'react-particles-js';
 import Navbar from "./components/Navbar";
 import Header from './components/Header';
 import Profile from './components/Profile';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
       <Navbar />
       <Header />
       <Profile />
+      <Services />
     </>
   );
 }
